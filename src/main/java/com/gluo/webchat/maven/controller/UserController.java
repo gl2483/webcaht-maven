47fadad90582ccce00000ef65a2303b832358e05
gl2483 <gl2483@columbia.edu>
2016-06-13 15:43:22 -0400

    added hibernate and util classes

---
+++ src/main/java/com/gluo/webchat/maven/controller/UserController.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gluo.webchat.maven.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import com.gluo.webchat.maven.model.ChatUser;
+
+/**
+ *
+ * @author luoguanqi
+ */
+@Controller
+@RequestMapping("/user/*")
+public class UserController{
+    
+    
+    @RequestMapping(value="CreateAccount", method=RequestMethod.POST)
+    @ResponseBody
+    public int CreateAccount(@RequestParam("username") String usrname, @RequestParam("password") String password){
+        return    ChatUser.createUser(usrname, password);
+
+    }
+}
---
+++ src/main/java/com/gluo/webchat/maven/model/ChatUser.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gluo.webchat.maven.model;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import com.gluo.webchat.maven.utilities.AES;
+import com.gluo.webchat.maven.utilities.HibernateUtil;
+
+/**
+ *
+ * @author KK
+ */
+public class ChatUser {
+    private int UserId;
+    private String Username;
+    private String Password;
+     
+    
+    public ChatUser(){}
+    public ChatUser(int userid, String name, String pwd){
+        UserId = userid;
+        Username = name;
+        Password = pwd;
+    }
+    
+    public ChatUser(String name, String pwd){
+        Username = name;
+        Password = pwd;
+    }
+    
+    public int getUserId(){
+        return UserId;
+    }
+    
+    public void setUserId(int id){
+        UserId = id;
+    }
+    
+    public String getUsername(){
+        return Username;
+    }
+    
+    public void setUsername(String name){
+        Username = name;
+    }
+    
+    public String getPassword(){
+        return Password;
+    }
+    
+    public void setPassword(String pwd){
+        try{
+            Password = AES.encrypt(pwd);
+        }
+        catch(Exception ex){
+            Logger.getLogger(ChatUser.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public static Integer createUser(String name, String pass){
+        SessionFactory factory = HibernateUtil.getSessionFactory();
+        Session session = factory.openSession();
+        Transaction tx = null;
+        Integer ret = null;
+        try{
+            tx = session.beginTransaction();
+            //ChatUser newuser = new ChatUser(name, pass);
+            //ret = (Integer)session.save(newuser);
+            tx.commit();
+        }
+        catch(HibernateException ex){
+            if (tx!=null) tx.rollback();
+        }
+        finally{
+            session.close();
+        }
+        return ret;
+    }
+    
+    
+    
+}
